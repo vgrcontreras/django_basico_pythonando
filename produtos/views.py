361ae06fa19da3086f63791efd2174ad0b13b345
Victor Contreras <contreras3991@gmail.com>
2024-09-11 20:43:19 -0300

    static files

--- produtos/views.py
+++ produtos/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def ver_produtos(request):
+    nome = 'victor'
+
     return render(
         request,
         template_name='ver_produto.html',
-        context={'nome': 'victor'})
+        context={'nome': nome}
+        )
 
 def inserir_produtos(request):
     return HttpResponse('inserir produtos')

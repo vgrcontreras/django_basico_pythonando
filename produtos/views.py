1ef87ad1213c83fa874167010f01f08253d17743
Victor Contreras <contreras3991@gmail.com>
2024-09-11 22:15:11 -0300

    migrations

--- produtos/views.py
+++ produtos/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pessoa
 
 # Create your views here.
 def ver_produtos(request):
-    nome = 'victor'
+    if request.method == 'GET':
+        nome = 'victor'
 
-    return render(
-        request,
-        template_name='ver_produto.html',
-        context={'nome': nome}
-        )
+        return render(
+            request,
+            template_name='ver_produto.html',
+            context={'nome': nome}
+            )
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+
+        pessoas = Pessoa.objects.filter(nome=nome)
+
+        if pessoas.exists():
+            return HttpResponse('Usuário já cadastrado')
+        else:
+            pessoa_db = Pessoa(nome=nome,
+                            idade=idade
+                            )
+            
+            pessoa_db.save()
+
+            return HttpResponse('Dados cadastrado com sucesso')
+
+
 
 def inserir_produtos(request):
     return HttpResponse('inserir produtos')
